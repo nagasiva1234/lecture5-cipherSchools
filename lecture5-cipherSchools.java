f08fac91c76f8a33f6d2c4ae90d78bbe4cba5747
nagasiva1234 <105148001+nagasiva1234@users.noreply.github.com>
2023-07-23 21:17:26 +0530

    Add files via upload

---
+++ lecture5-cipherSchools.java
@@ -0,0 +1,34 @@
+public class Operators {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+        System.out.println("x is: " + x);
+        System.out.println("y is: " + y);
+
+        byte b = 10;
+        System.out.println("B is: " + b);
+
+        byte b2 = (byte) 129;
+        System.out.println("B2 is: " + b2);
+
+        byte b3 = (byte) 128;
+        System.out.println("B3 is: " + b3);
+
+        byte b4 = (byte) 256;
+        System.out.println("B4 is: " + b4);
+
+        float f = (float) 5.5; // by default considers decimal to be double
+
+        float f2 = 5.5f;
+
+        double d = 10.5;
+
+        char ch = 'a';
+        System.out.println(ch);
+
+        int z = x + y;
+        // + -> operator
+        // x,y -> operands
+    }
+
+}
